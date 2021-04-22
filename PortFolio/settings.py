fe6da264cb7a10333bdc108de55b8701756e66e4
Avinash Kumar <44208272+avinashmallik62@users.noreply.github.com>
2021-04-23 02:50:40 +0530

    Update settings.py

--- PortFolio/settings.py
+++ PortFolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x)vir5n+qkmw@*)v3eeoynvp$+8f3wqg-@(05=d#l!sunu@b-k'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
